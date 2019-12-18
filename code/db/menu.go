aa02fc0837608b11170913fb14eed87dce103717
frontlon <front_diablo@163.com>
2019-12-18 23:20:12 +0800

    menu sort

--- code/db/menu.go
+++ code/db/menu.go
@@ -7,11 +7,11 @@ import (
 	"strings"
 )
 
-
 type Menu struct {
 	Name     string
 	Platform uint32
-	Pinyin string
+	Pinyin   string
+	Sort     uint32
 }
 
 //写入cate数据
@@ -21,7 +21,7 @@ func (v *Menu) Add() error {
 		fmt.Println(err.Error())
 		return err
 	}
-	_, err = stmt.Exec(v.Name,v.Platform,v.Pinyin);
+	_, err = stmt.Exec(v.Name, v.Platform, v.Pinyin);
 	if err != nil {
 		fmt.Println(err.Error())
 		return err
@@ -39,7 +39,7 @@ func (*Menu) GetByPlatform(platform uint32) ([]*Menu, error) {
 	if platform != 0 {
 		where += " WHERE platform = " + utils.ToString(platform)
 	}
-	sql := "SELECT name,platform FROM menu " + where + " ORDER BY pinyin ASC"
+	sql := "SELECT name,platform FROM menu " + where + " ORDER BY sort ASC,pinyin ASC"
 
 	rows, err := sqlite.Query(sql)
 	if err != nil {
@@ -57,13 +57,13 @@ func (*Menu) GetByPlatform(platform uint32) ([]*Menu, error) {
 }
 
 //删除一个平台下不存在的目录
-func (sim *Menu) DeleteNotExists(platform uint32,menus []string) (error) {
+func (*Menu) DeleteNotExists(platform uint32, menus []string) (error) {
 
 	sql := ""
 	//如果没有菜单，则全部删除
 	if len(menus) == 0 {
 		sql = "DELETE FROM menu WHERE platform = " + utils.ToString(platform)
-	}else{
+	} else {
 		menuStr := strings.Join(menus, "\",\"")
 		menuStr = "\"" + menuStr + "\""
 		sql = "DELETE FROM menu WHERE platform = " + utils.ToString(platform) + " AND name not in (" + menuStr + ")"
@@ -77,7 +77,7 @@ func (sim *Menu) DeleteNotExists(platform uint32,menus []string) (error) {
 }
 
 //删除不存在的平台下的所有menu
-func (sim *Menu) ClearByPlatform(platforms []string) (error) {
+func (*Menu) ClearByPlatform(platforms []string) (error) {
 
 	sql := "DELETE FROM menu "
 
@@ -94,9 +94,8 @@ func (sim *Menu) ClearByPlatform(platforms []string) (error) {
 	return nil
 }
 
-
 //根据一组名称，查询存在的名称，用于取交集
-func (sim *Menu) GetMenuByNames(platform uint32, names []string) ([]string,error) {
+func (*Menu) GetMenuByNames(platform uint32, names []string) ([]string, error) {
 
 	nameStr := strings.Join(names, "\",\"")
 	nameStr = "\"" + nameStr + "\""
@@ -117,10 +116,28 @@ func (sim *Menu) GetMenuByNames(platform uint32, names []string) ([]string,error
 }
 
 //清空表数据
-func (sim *Menu) Truncate() (error) {
+func (*Menu) Truncate() (error) {
 	_, err := sqlite.Exec("DELETE FROM menu")
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+//更新排序
+func (menu *Menu) UpdateSortByName() error {
+	sql := `UPDATE menu SET `
+	sql += `sort = ` + utils.ToString(menu.Sort)
+	sql += ` WHERE name = '` + utils.ToString(menu.Name) + `'`
+
+	stmt, err := sqlite.Prepare(sql)
+	if err != nil {
+		return err
+	}
+	_, err2 := stmt.Exec()
+	if err2 != nil {
+		return err2
+	} else {
+		return nil
+	}
 }
--- code/viewFunc.go
+++ code/viewFunc.go
@@ -337,6 +337,32 @@ func defineViewFunction(w *window.Window) {
 		return sciter.NewValue(string(jsonRom))
 	})
 
+	//更新菜单排序
+	w.DefineFunction("UpdateMenuSort", func(args ...*sciter.Value) *sciter.Value {
+		platform := uint32(utils.ToInt(args[0].String())) //平台
+		data := args[1].String()
+
+		d := make(map[string]uint32)
+		json.Unmarshal([]byte(data), &d)
+
+		if len(d) == 0 {
+			return sciter.NullValue()
+		}
+
+		for name, val := range d {
+			if name == ""{
+				continue
+			}
+			menu := &db.Menu{
+				Name:     name,
+				Platform: platform,
+				Sort:     val,
+			}
+			if err := menu.UpdateSortByName();err != nil{}
+		}
+		return sciter.NewValue("1")
+	})
+
 	//读取游戏数量
 	w.DefineFunction("GetGameCount", func(args ...*sciter.Value) *sciter.Value {
 		platform := uint32(utils.ToInt(args[0].String()))
