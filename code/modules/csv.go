8045b45de5e8e684878439b627dbc39a01e16323
frontlon <front_diablo@163.com>
2020-05-23 16:05:17 +0800

    csv读写功能

---
+++ code/modules/csv.go
@@ -0,0 +1,61 @@
+package modules
+
+import (
+	"simUI/code/utils"
+)
+
+type CsvStruct struct {
+	RomName   string //rom文件名
+	EnName    string //英文名
+	CnName    string //中文名
+	Type      string //类型
+	Platform  string //平台
+	Year      string //年份
+	Developer string //开发商
+	Publisher string //发行商
+}
+
+//读取详情文件
+func ReadDescFile(platform uint32) (map[string]*CsvStruct, error) {
+	csvData := map[string]*CsvStruct{}
+
+	filename := "1.csv"
+	records, err := utils.ReadCsv(filename)
+	if err != nil {
+		return nil, err
+	}
+	for _, r := range records {
+		csvData[r[0]] = &CsvStruct{
+			RomName:   r[0],
+			EnName:    r[1],
+			CnName:    r[2],
+			Type:      r[3],
+			Platform:  r[4],
+			Year:      r[5],
+			Developer: r[6],
+			Publisher: r[7],
+		}
+	}
+
+	return csvData, nil
+}
+
+func WriteDescFile(platform uint32,newData *CsvStruct) error {
+	filename := "1.csv"
+
+	data, _ := ReadDescFile(platform) //读取老数据
+	data[newData.RomName] = newData //并入新数据
+
+	//转换为切片
+	create := [][]string{}
+	for _, v := range data {
+		d := []string{v.RomName, v.EnName, v.CnName, v.Type, v.Platform, v.Year, v.Developer, v.Publisher}
+		create = append(create, d)
+	}
+
+	if err := utils.WriteCsv(filename, create); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ code/utils/csv.go
@@ -0,0 +1,58 @@
+package utils
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
+
+//读取csv
+func ReadCsv(filename string) ([][]string, error) {
+
+	if filename == "" {
+		return nil, nil
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, err
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	data := [][]string{}
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Error:", err)
+			return nil, err
+		}
+		data = append(data, record)
+	}
+	return data, nil
+}
+
+//写入csv
+func WriteCsv(filename string, data [][]string) error {
+
+	if filename == "" {
+		return nil
+	}
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		fmt.Println("open file is failed, err: ", err)
+		return err
+	}
+	defer file.Close()
+	file.WriteString("\xEF\xBB\xBF")
+	w := csv.NewWriter(file)
+	for _, d := range data {
+		w.Write(d)
+	}
+	w.Flush()
+	return nil
+}
