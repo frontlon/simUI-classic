ad50354382f8644920bad0ddebcebf1f1e93d830
frontlon <front_diablo@163.com>
2020-05-26 15:27:31 +0800

    sql升级功能

--- code/db/db.go
+++ code/db/db.go
@@ -1,11 +1,13 @@
 package db
 
 import (
-	"simUI/code/utils"
 	"errors"
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"io/ioutil"
+	"os"
+	"simUI/code/utils"
 )
 
 var engine *gorm.DB
@@ -15,7 +17,7 @@ func Conn() error {
 	//连接数据库
 	err := errors.New("")
 
-	if !utils.FileExists("data.dll"){
+	if !utils.FileExists("data.dll") {
 		fmt.Println("数据库文件data.dll不存在")
 		return errors.New("数据库文件data.dll不存在\n Database does not exist")
 	}
@@ -36,8 +38,54 @@ func getDb() *gorm.DB {
 	return engine
 }
 
-
 //收缩数据库
-func Vacuum(){
+func Vacuum() {
 	engine.Exec("VACUUM;")
+}
+
+//升级数据库
+func UpgradeDB() {
+
+	filename := "upgrade.dat"
+
+	if !utils.FileExists(filename){
+		return
+	}
+
+
+	/*f, err := os.Open(filename)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	defer os.Remove(filename)
+
+	rd := bufio.NewReader(f)
+	for {
+		line, err := rd.ReadString('\n') //以'\n'为结束符读入一行
+		if err != nil || io.EOF == err {
+			break
+		}
+		//alter table mydownload add column 'IsFree' varchar(100) default '1'
+		getDb().Exec(line)
+	}*/
+
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("read file fail", err)
+		return
+	}
+	defer os.Remove(filename)
+	defer f.Close()
+
+	sql, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println("read to fd fail", err)
+		return
+	}
+	getDb().Exec(string(sql))
+
+	return
+
+
 }
