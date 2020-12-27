13c592d2e9d8017048ae93e8e8088d1eea6a33d1
frontlon\Administrator <front_diablo@163.com>
2020-12-27 15:44:31 +0800

    resize

--- code/main.go
+++ code/main.go
@@ -62,6 +62,7 @@ func main() {
 	//创建window窗口
 	w, err := window.New(
 		sciter.SW_MAIN|
+			sciter.SW_RESIZEABLE|
 			sciter.SW_ENABLE_DEBUG,
 		&sciter.Rect{Left: 0, Top: 0, Right: int32(utils.ToInt(width)), Bottom: int32(utils.ToInt(height))})
 	if err != nil {
@@ -153,4 +154,3 @@ func newHandler(s *sciter.Sciter) *sciter.CallbackHandler {
 		OnLoadData: OnLoadData(s),
 	}
 }
-
